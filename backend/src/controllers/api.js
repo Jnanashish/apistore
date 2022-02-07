9a77922832d1ef36c546ef25f9722c0b4d20dfd4
Jnanashish <jhandique1999@gmail.com>
2022-02-07 23:10:35 +0530

    remove logs

--- backend/src/controllers/api.js
+++ backend/src/controllers/api.js
@@ -30,20 +30,21 @@ exports.getapi = (req, res) => {
 
 // add new api to database
 exports.addapi = (req, res) =>{
-    console.log(req.files);
     const {title, desc, author, github, endpoint} = req.body;
     const file = req.files.photo;
     cloudinary.uploader.upload(file.tempFilePath, 
         { transformation: { width: 260, height: 160, crop: "pad" }},              
         (err, result) => {  
-        console.log(result);    
+    
         const data = new Api({title, desc, author, github, endpoint, photo : result.secure_url})
         data.save((err, result) => {
             if(err){
+                console.log(err);
                 return res.status(500).json({
                     error : "Cannot add data"
                 })            
             } 
+            
             return res.status(201).json({
                 message : "Data added successfully"
             })         
